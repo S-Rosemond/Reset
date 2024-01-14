bdccaac8fdd1ce4240487a9f830a8beec9ce346e
Samuel <JS.Rosemond@gmail.com>
2024-01-13 19:57:40 -0500

    truth check

---
+++ truthCheck.js
@@ -0,0 +1,26 @@
+function truthCheck(collection, pre) {
+  pre = collection.every((el) => !!el[pre] === true);
+
+  return pre;
+}
+
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "isBot"
+  )
+);
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "name"
+  )
+);
