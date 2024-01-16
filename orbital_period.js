0a9d5663b340cd3d8bf365ed06156ca252e1ec1d
Samuel <JS.Rosemond@gmail.com>
2024-01-16 05:19:25 -0500

    Orbital Period

---
+++ orbital_period.js
@@ -0,0 +1,25 @@
+function calcOrbitalPeriod(avgAlt) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const twoPI = 2 * Math.PI;
+  const semiMajorAxis = avgAlt + earthRadius;
+  const semiMajorAxisCube = Math.pow(semiMajorAxis, 3);
+
+  const squareRootADivU = Math.sqrt(semiMajorAxisCube / GM);
+
+  const orbitalPeriod = Math.round(twoPI * squareRootADivU);
+
+  return orbitalPeriod;
+}
+
+function orbitalPeriod(arr) {
+  const result = arr.map((el) => {
+    const orbitalPeriod = calcOrbitalPeriod(el.avgAlt);
+
+    return { name: el.name, orbitalPeriod };
+  });
+  console.log(result);
+  return result;
+}
+
+orbitalPeriod([{ name: "sputnik", avgAlt: 35873.5553 }]);
